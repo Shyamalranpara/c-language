8bcfcb3cabfa9388601c885343f1f194b96b9d93
shyamalranpara <shyamalranpara7846@gmail.com>
2024-03-22 13:10:45 +0530

    looping pattern-1

---
+++ Looping pattern-1/task 6.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+main(){
+
+    for (int i = 1; i <= 5; i++)
+    {
+
+        for (int j = 1; j <= 6-i; j++)
+        {
+            printf("%d ",j % 2);
+        }
+
+        printf("\n");
+    }
+}   
